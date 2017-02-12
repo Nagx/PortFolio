2348de48123cf4b234d5c6597dc27a140d2b4c03
Nagx <k.javazzo@gmail.com>
2017-02-12 15:32:32 +0100

    create the LoginType

---
+++ src/AdminBundle/Form/LoginType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+        ;
+    }
+}
