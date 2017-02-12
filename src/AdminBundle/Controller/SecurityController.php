5857aca77b902203765ee0c3cd254331632bb953
Nagx <k.javazzo@gmail.com>
2017-02-12 16:37:58 +0100

    fix SecurityController error name

--- src/AdminBundle/Controller/SecurityController.php
+++ src/AdminBundle/Controller/SecurityController.php
@@ -6,23 +6,11 @@ use AdminBundle\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class SecutiryController extends Controller
+class SecurityController extends Controller
 {
     public function loginAction(Request $request)
     {
         $form = $this->createForm(LoginType::class);
-        $user = $this->getUser();
-
-        if (null == $user) {
-            Throw $this->createAccessDeniedException();
-        }
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-        }
-
         return $this->render('Admin/pages/login.html.twig', array('form' => $form->createView()));
     }
 }
