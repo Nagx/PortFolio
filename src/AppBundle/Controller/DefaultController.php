b84d0f8cef3bdef16c5aefc1e1dd7c01020b9432
Nagx <k.javazzo@gmail.com>
2017-04-15 14:14:05 +0200

    restart project for V1

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,12 +2,24 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        /*$em = $this->getDoctrine()->getManager();
+        $user = new User();
+        $user->setFirstname('kevin')
+            ->setLastname('javazzo');
+        $encoder = $this->container->get('security.password_encoder');
+        $password = $encoder->encodePassword($user, 'Naga2909');
+        $user->setPassword($password)
+            ->setEmail('k.javazzo.pro@gmail.com')
+            ->setRoles('ROLE_ADMIN');
+        $em->persist($user);
+        $em->flush();*/
         return $this->render('AppBundle:Default:index.html.twig');
     }
 }
